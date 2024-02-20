a6b0c956ec38ba9bede29aed738782abf8336cd7
AshkanArabim <ashkan.arabim@gmail.com>
2024-02-19 18:23:40 -0700

    automate audio file search

--- demo.py
+++ demo.py
@@ -1,31 +1,27 @@
 from resemblyzer import VoiceEncoder, preprocess_wav
 import numpy as np
 from itertools import product
+import os
 
 
-encoder = VoiceEncoder()
+if __name__ == "__main__":
+    encoder = VoiceEncoder()
 
-voices = {
-    'ali 1': './ali 1.wav',
-    'ashkan 1': './ashkan 1.wav',
-    'ashkan 2': './ashkan 2.wav'
-}
+    # make dictionary of audio files
+    # note: put audio files in the root directory, and make sure they're `.wav` files
+    voices = {filename[:-4]: filename for filename in os.listdir('.') if ".wav" in filename}
 
-# load and preprocess audio tracks
-for name in voices:
-    voices[name] = preprocess_wav(voices[name])
+    # load and preprocess audio tracks
+    for name in voices:
+        voices[name] = preprocess_wav(voices[name])
 
-# # embed utterances (semantics of audio)
-# for name in voices:
-#     voices[name] = encoder.embed_utterance(voices[name])
+    # embed speaker (actual physical voice)
+    for name in voices:
+        voices[name] = encoder.embed_speaker([voices[name]])
 
-# embed speaker (actual physical voice)
-for name in voices:
-    voices[name] = encoder.embed_speaker([voices[name]])
-
-people_combos = {frozenset((s1, s2)) for s1, s2 in product(voices.keys(), voices.keys()) if s1 != s2}
-    
-for p1, p2 in people_combos:
-    similarity = np.dot(voices[p1], voices[p2])  # using dot product
-    # similarity = np.dot(p1, p2) /  # using cosine similarity
-    print(f"similarity of {p1} and {p2}:\t{similarity}")
+    people_combos = {frozenset((s1, s2)) for s1, s2 in product(voices.keys(), voices.keys()) if s1 != s2}
+        
+    for p1, p2 in people_combos:
+        similarity = np.dot(voices[p1], voices[p2])  # using dot product
+        # similarity = np.dot(p1, p2) /  # using cosine similarity
+        print(f"similarity of {p1} and {p2}:\t{similarity}")
