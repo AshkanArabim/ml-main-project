f3f345f777a5f6cc1fb20b65c3d73e7465304bb6
AshkanArabim <ashkan.arabim@gmail.com>
2024-03-12 01:10:28 -0600

    reduce model size

--- mnist_training.py
+++ mnist_training.py
@@ -5,6 +5,7 @@ from torchvision.datasets import FashionMNIST
 from torch.utils.data import DataLoader
 from torch import nn
 from torch import optim
+from torchsummary import summary
 
 
 # housework
@@ -30,29 +31,30 @@ class Model(nn.Module):
     def __init__(self, num_classes: int = 10, dropout: float = 0.5):
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=2),
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(32, 64, kernel_size=3, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
             nn.Dropout(p=dropout),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
+            # nn.Linear(256 * 6 * 6, 4096),
+            # nn.ReLU(inplace=True),
+            # # nn.Dropout(p=dropout),
+            # # nn.Linear(4096, 4096),
+            # # nn.ReLU(inplace=True),
+            # nn.Linear(4096, num_classes),
+            nn.Linear(256 * 6 * 6, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -64,12 +66,17 @@ class Model(nn.Module):
 
 
 # train model
-print("training...")
 model = Model().to(device)
 optimizer = optim.Adam(model.parameters())
 loss_func = nn.CrossEntropyLoss()
 
-for epoch in range(200):
+print('summary:')
+print(summary(model, (1, 28, 28)))
+
+print("training...")
+epochs = 100
+for epoch in range(epochs):
+    print(f'epoch {epoch} or {epochs} ------------------------------------')
     for i, data in enumerate(ds_train):
         inputs, labels = data
         inputs = inputs.to(device)
@@ -79,8 +86,8 @@ for epoch in range(200):
         batch_loss.backward()
         optimizer.step()
         
-        if i % 10000 == 0:
-            print(f'epoch {i} ------------------------------------')
+        if i % 100 == 0:
+            print(f'batch {i} of {len(ds_train)} ---------------')
             print(f'loss=\t{batch_loss}')
             print(f'accuracy=\t{torch.sum(torch.argmax(pred) == labels) / len(labels)}')
             
