c77c89ae83799417aade1b94cb1d02ee22b06823
James <jnewson.miners.utep.edu>
2024-04-15 16:56:44 -0600

    Updated -v

--- mnist_training.py
+++ mnist_training.py
@@ -28,20 +28,20 @@ ds_test = DataLoader(ds_test, batch_size=64, shuffle=True)
 
 # define model
 class Model(nn.Module):
-    def __init__(self, num_classes: int = 10, dropout: float = 0.5):
+    def __init__(self, num_classes: int = 10, dropout: float = 0.10):
         super().__init__()
         self.features = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
@@ -54,7 +54,7 @@ class Model(nn.Module):
             # # nn.Linear(4096, 4096),
             # # nn.ReLU(inplace=True),
             # nn.Linear(4096, num_classes),
-            nn.Linear(32 * 6 * 6, num_classes),
+            nn.Linear(256 * 6 * 6, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -97,9 +97,7 @@ print("finished training.")
 # save model
 # eh...
 
-# Working on it -Ale
-
-
+# working on it -Ale
 
 # make prediction
 print('evaluating performance...')
