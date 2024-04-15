18d8ad24ebb8b406fb9257d19179776382c28403
AleLeeHrtz <ale@MacBook-Air-de-Alejandra.local>
2024-04-15 15:31:35 -0600

    Test

--- mnist_training.py
+++ mnist_training.py
@@ -34,14 +34,14 @@ class Model(nn.Module):
             nn.Conv2d(1, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=2),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
@@ -54,7 +54,7 @@ class Model(nn.Module):
             # # nn.Linear(4096, 4096),
             # # nn.ReLU(inplace=True),
             # nn.Linear(4096, num_classes),
-            nn.Linear(256 * 6 * 6, num_classes),
+            nn.Linear(32 * 6 * 6, num_classes),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
