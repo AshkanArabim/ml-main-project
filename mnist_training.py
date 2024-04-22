fix accuracy calculation

mnist_training.py
@@ -1,5 +1,5 @@
 import torch
-# from torchvision.transforms import functional
+from torch.nn import functional
 from torchvision import transforms
 from torchvision.datasets import FashionMNIST
 from torch.utils.data import DataLoader
@@ -22,52 +22,39 @@ transform = transforms.Compose([
 ds_train = FashionMNIST('.', download=True, train=True, transform=transform)
 ds_test = FashionMNIST('.', download=True, train=False, transform=transform)
 
-ds_train = DataLoader(ds_train, batch_size=64, shuffle=True)
-ds_test = DataLoader(ds_test, batch_size=64, shuffle=True)
+ds_train = DataLoader(ds_train, batch_size=2 ** 13, shuffle=True)
+ds_test = DataLoader(ds_test, batch_size=2 ** 13, shuffle=True)
 
-
-# define model
 class Model(nn.Module):
-    def __init__(self, num_classes: int = 10, dropout: float = 0.5):
-        super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=dropout),
-            # nn.Linear(256 * 6 * 6, 4096),
-            # nn.ReLU(inplace=True),
-            # # nn.Dropout(p=dropout),
-            # # nn.Linear(4096, 4096),
-            # # nn.ReLU(inplace=True),
-            # nn.Linear(4096, num_classes),
-            nn.Linear(256 * 6 * 6, num_classes),
-        )
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.fc1 = nn.Linear(1152, 512)
+        self.fc2 = nn.Linear(512, 10)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.features(x)
-        x = self.avgpool(x)
+    def forward(self, x):
+        x = functional.relu(self.conv1(x))
+        x = self.pool(x)
+        x = functional.relu(self.conv2(x))
+        x = self.pool(x)
+        x = functional.relu(self.conv3(x))
+        x = self.pool(x)
+        
         x = torch.flatten(x, 1)
-        x = self.classifier(x)
+        
+        x = functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        
         return x
-
 
 # train model
 model = Model().to(device)
-optimizer = optim.Adam(model.parameters())
+optimizer = optim.Adam(model.parameters(), lr=1e-5)
 loss_func = nn.CrossEntropyLoss()
 
 print('summary:')
@@ -86,10 +73,10 @@ for epoch in range(epochs):
         batch_loss.backward()
         optimizer.step()
         
-        if i % 100 == 0:
+        if i % 10 == 0:
             print(f'batch {i} of {len(ds_train)} ---------------')
             print(f'loss=\t{batch_loss}')
-            print(f'accuracy=\t{torch.sum(torch.argmax(pred) == labels) / len(labels)}')
+            print(f'accuracy=\t{torch.sum(torch.argmax(pred, dim=1) == labels) / len(labels)}')
             
 print("finished training.")
 
@@ -107,7 +94,7 @@ for data in ds_test:
     inputs = inputs.to(device)
     labels = labels.to(device)
     pred = model(inputs)
-    total_correct_preds += torch.sum(torch.argmax(pred) == labels)
+    total_correct_preds += torch.sum(torch.argmax(pred, dim=1) == labels)
     total_datapoints += len(labels)
     
 print(f'accuracy=\t{total_correct_preds / total_datapoints}')