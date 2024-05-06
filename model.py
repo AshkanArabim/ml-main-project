3c104c1afa2596ccf95c7d8359ec7badc7501353
AshkanArabim <ashkan.arabim@gmail.com>
2024-05-05 19:17:49 -0600

    improve model

--- model.py
+++ model.py
@@ -5,26 +5,48 @@ from torch.nn import functional
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv1_1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
+        self.conv1_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.bnorm1 = nn.BatchNorm2d(64)
+        
+        self.conv2_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv2_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.bnorm2 = nn.BatchNorm2d(128)
+        
+        self.conv3_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.bnorm3 = nn.BatchNorm2d(256)
         
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.fc1 = nn.Linear(1152, 512)
+        self.relu = functional.relu
+        
+        self.dropout = nn.Dropout() # defaults to a probability of 0.5
+        
+        self.fc1 = nn.Linear(2304, 512)
         self.fc2 = nn.Linear(512, 10)
 
     def forward(self, x):
-        x = functional.relu(self.conv1(x))
+        x = self.relu(self.conv1_1(x))
+        x = self.relu(self.conv1_2(x))
+        x = self.bnorm1(x)
         x = self.pool(x)
-        x = functional.relu(self.conv2(x))
+        
+        x = self.relu(self.conv2_1(x))
+        x = self.relu(self.conv2_2(x))
+        x = self.bnorm2(x)
         x = self.pool(x)
-        x = functional.relu(self.conv3(x))
+        
+        x = self.relu(self.conv3_1(x))
+        x = self.relu(self.conv3_2(x))
+        x = self.bnorm3(x)
         x = self.pool(x)
         
         x = torch.flatten(x, 1)
         
+        x = self.dropout(x)
         x = functional.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         
         return x
